f8ffd1d607ef5a191c7e2b391dc514894cc7e929
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-01-01 17:33:31 +0330

    add impl resolve ref/owned for object in macro

--- derive/src/args/object.rs
+++ derive/src/args/object.rs
@@ -34,11 +34,40 @@ fn impl_object(object: &Object) -> TokenStream {
     }
 }
 
+fn impl_resolve_owned(object: &Object) -> TokenStream {
+    let ident = &object.ident;
+    let create_name = get_create_name();
+    quote! {
+        impl<'a> #create_name::ResolveOwned<'a> for #ident {
+            fn resolve_owned(self, _ctx: &#create_name::Context) -> #create_name::Result<Option<#create_name::FieldValue<'a>>> {
+                Ok(Some(#create_name::FieldValue::owned_any(self)))
+            }
+        }
+    }
+}
+
+fn impl_resolve_ref(object: &Object) -> TokenStream {
+    let ident = &object.ident;
+    let create_name = get_create_name();
+    quote! {
+        impl<'a> #create_name::ResolveRef<'a> for #ident {
+            fn resolve_ref(&'a self, _ctx: &#create_name::Context) -> #create_name::Result<Option<#create_name::FieldValue<'a>>> {
+                Ok(Some(#create_name::FieldValue::borrowed_any(self)))
+            }
+        }
+    }
+}
+
 impl ToTokens for Object {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let impl_object = impl_object(self);
+        let impl_resolve_owned = impl_resolve_owned(self);
+        let impl_resolve_ref = impl_resolve_ref(self);
+
         tokens.extend(quote! {
             #impl_object
+            #impl_resolve_owned
+            #impl_resolve_ref
         });
     }
 }
