7f471f3e6d1189491caa2e89be7b368b79622585
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2023-03-04 15:06:58 +0330

    add custom type test

--- derive/tests/registry.rs
+++ derive/tests/registry.rs
@@ -1,8 +1,19 @@
-use dynamic_graphql::internal::Registry;
-use dynamic_graphql::SimpleObject;
-use dynamic_graphql::{dynamic, value};
-
 use crate::schema_utils::normalize_schema;
+use dynamic_graphql::dynamic;
+use dynamic_graphql::dynamic::DynamicRequestExt;
+use dynamic_graphql::internal::Object;
+use dynamic_graphql::internal::OutputTypeName;
+use dynamic_graphql::internal::ParentType;
+use dynamic_graphql::internal::Register;
+use dynamic_graphql::internal::Registry;
+use dynamic_graphql::internal::ResolveOwned;
+use dynamic_graphql::internal::ResolveRef;
+use dynamic_graphql::internal::TypeName;
+use dynamic_graphql::value;
+use dynamic_graphql::App;
+use dynamic_graphql::Context;
+use dynamic_graphql::SimpleObject;
+use std::borrow::Cow;
 
 mod schema_utils;
 
@@ -59,4 +70,101 @@ async fn test_apply() {
         .into_result()
         .unwrap();
     assert_eq!(result.data, value!({ "foo": { "value": "the foo" } }));
+}
+
+#[tokio::test]
+async fn define_custom_type() {
+    // define Foo type
+    struct Foo {
+        value: String,
+    }
+
+    impl TypeName for Foo {
+        fn get_type_name() -> Cow<'static, str> {
+            "Foo".into()
+        }
+    }
+
+    impl ParentType for Foo {
+        type Type = Foo;
+    }
+    impl OutputTypeName for Foo {}
+    impl Object for Foo {}
+    impl Register for Foo {
+        fn register(registry: Registry) -> Registry {
+            let object = dynamic::Object::new("Foo");
+            let object = object.field(dynamic::Field::new(
+                "value",
+                dynamic::TypeRef::named_nn("String"),
+                |ctx| {
+                    let query: &Foo = ctx.parent_value.downcast_ref().unwrap();
+                    dynamic::FieldFuture::new(async move {
+                        Ok(Some(dynamic::FieldValue::value(query.value.clone())))
+                    })
+                },
+            ));
+            registry.register_type(object)
+        }
+    }
+
+    impl<'a> ResolveOwned<'a> for Foo {
+        fn resolve_owned(
+            self,
+            _ctx: &Context,
+        ) -> dynamic_graphql::Result<Option<dynamic::FieldValue<'a>>> {
+            Ok(Some(dynamic::FieldValue::owned_any(self)))
+        }
+    }
+    impl<'a> ResolveRef<'a> for Foo {
+        fn resolve_ref(
+            &'a self,
+            _ctx: &Context,
+        ) -> dynamic_graphql::Result<Option<dynamic::FieldValue<'a>>> {
+            Ok(Some(dynamic::FieldValue::borrowed_any(self)))
+        }
+    }
+
+    // use Foo type
+
+    #[derive(SimpleObject)]
+    #[graphql(root)]
+    struct Query {
+        foo: Foo,
+    }
+
+    #[derive(App)]
+    struct App(Query);
+
+    let schema = App::create_schema().finish().unwrap();
+
+    // use schema
+    let sdl = schema.sdl();
+    assert_eq!(
+        normalize_schema(&sdl),
+        normalize_schema(
+            r#"
+            type Foo {
+              value: String!
+            }
+
+            type Query {
+              foo: Foo!
+            }
+
+            schema {
+              query: Query
+            }
+            "#
+        ),
+    );
+
+    let root = Query {
+        foo: Foo {
+            value: "the foo".to_string(),
+        },
+    };
+    let query = "{ foo { value } }";
+    let req = dynamic_graphql::Request::new(query).root_value(dynamic::FieldValue::owned_any(root));
+    let res = schema.execute(req).await;
+    assert_eq!(res.data, value!({ "foo": { "value": "the foo" } }));
 }
